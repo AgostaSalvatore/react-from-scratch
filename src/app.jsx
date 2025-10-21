049019b5d0e64e92ac993502a39ef6f2233d9d3b
AgostaSalvatore <agosta.boolean@gmail.com>
2025-10-21 04:40:13 +0200

    feat: add theme styling and dynamic tech variable to App component

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,15 @@
 import ReactDOM from "react-dom/client";
+import "./app.css";
+
+const tech = "React and Vite";
+const theme = {
+    primary: 'rebeccaPurple',
+    secondary: 'paleVioletRed'
+}
 
 function App() {
     return (
-        <h1>Hello React and Vite!</h1>
+        <h1 style={{ backgroundColor: theme.primary, color: theme.secondary }}>Hello {tech}</h1>
     )
 }
 
