6d9e487fb880b07c708177b8577ff7d51662ab61
AgostaSalvatore <agosta.boolean@gmail.com>
2025-10-22 07:53:08 +0200

    feat: add main layout with search, shortlist, puppies list and new puppy form components without logic

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,35 @@ import { PageWrapper } from "./components/PageWrapper";
 import { Container } from "./components/Container";
 import { Header } from "./components/Header";
 
+// Main Components
+import { PuppiesList } from "./components/PuppiesList";
+import { Shortlist } from "./components/Shortlist";
+import { Search } from "./components/Search";
+import { NewPuppyForm } from "./components/NewPuppyForm";
+
 export function App() {
     return (
         <PageWrapper>
 
             <Container>
                 <Header />
+                <Main />
             </Container>
 
         </PageWrapper>
+    )
+}
+
+function Main() {
+    return (
+        <>
+            <div className="mt-24 grid gap-8 sm:grid-cols-2">
+                <Search />
+                <Shortlist />
+            </div>
+            <PuppiesList />
+            <NewPuppyForm />
+        </>
+
     )
 }
